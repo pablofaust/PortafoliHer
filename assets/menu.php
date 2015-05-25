1ecd140aea78246c8c57ec4d44e3b780d2381ac4
Pablo Faust <pabfaust@gmail.com>
2015-05-25 00:49:54 -0500

    Heroku

---
+++ assets/menu.php
@@ -0,0 +1,7 @@
+<div class="container-fluid menu">
+
+	<div class="navbar">
+
+	</div>
+
+</div>
---
+++ home/background.php
@@ -0,0 +1,23 @@
+<div class="container-fluid background">
+
+	<div class="background-1">
+
+		<div class="background-2">
+
+			<div class="background-3">
+
+				<div class="background-4">
+
+					<div class="background-5 hidden-xs">
+
+					</div>		
+
+			</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>	
